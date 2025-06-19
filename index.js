e772045194ed1def02e9e999987ca73903147383
Clint Zold <214383878+clintzold@users.noreply.github.com>
2025-06-19 10:52:36 -0600

    Create function to get human choice
    GET human choice of the strings 'ROCK', 'PAPER' or
    'SCISSORS' using prompt.
    
    Make the input capitalized.
    
    Test value of humanChoice with console.logs

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 //Global scope variables
 let randNum;
 let computerChoice;
+let humanChoice;
 
 //GETS computer choice of ROCK/PAPER/SCISSORS
 function getComputerChoice() {
@@ -15,7 +16,13 @@ function getComputerChoice() {
     } else {
         computerChoice = 'SCISSORS';
     };
-    console.log(computerChoice);
 };
 
-getComputerChoice();
+//GETS user choice of ROCK/PAPER/SCISSORS
+function getHumanChoice() {
+    let humanString = prompt('Enter ROCK, PAPER or SCISSORS: ');
+    humanChoice = humanString.toUpperCase();
+    console.log(humanChoice);
+};
+
+getHumanChoice();
